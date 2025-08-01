540604314d39ee25a61a0ab784413b55e91cb6d8
Denisowiec <ddspeed@gmail.com>
2025-08-01 18:21:28 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    example = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(example)
+
+if __name__=="__main__":
+    main()
